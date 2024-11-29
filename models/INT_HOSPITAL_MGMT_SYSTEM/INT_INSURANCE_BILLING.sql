99cd57edab0317720190ea333da382d859c5c4f6
Deepu Vakkalagadda <thesuperstardeepu@gmail.com>
2024-11-29 01:05:56 +0000

    coomit

--- models/INT_HOSPITAL_MGMT_SYSTEM/INT_INSURANCE_BILLING.sql
+++ models/INT_HOSPITAL_MGMT_SYSTEM/INT_INSURANCE_BILLING.sql
@@ -1,3 +1,16 @@
-{{ config(materialized='view') }}
-select * from 
-{{ ref('INT_EMP_INSURANCE_BILLING') }} ;
+    {{ config(materialized='view') }}
+    SELECT 
+    a.surrogate_key,
+    a.insurance_id,
+    a.insurance_name,
+    a.policy_number,
+    a.policy_holder,
+    a.coverage_start_date,
+    a.coverage_end_date,
+    a.monthly_premium,
+    b.hospital_id,
+    b.hospital_name
+    FROM {{ ref('INT_EMP_INSURANCE_BILLING') }}  a
+    left outer join
+    {{ ref('INT_HOSPITAL_PHYSICANS') }} b
+    on a.insurance_id=b.insurance_id;
