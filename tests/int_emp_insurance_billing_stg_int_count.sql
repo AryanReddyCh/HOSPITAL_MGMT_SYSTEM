3bb8add3245fef7096ee839a000fd5322fac78ba
MANIKANTA SAI DEEPU VAKKALAGADDA <thesuperstardeepu@gmail.com>
2024-12-05 20:52:43 +0000

    commit

--- tests/int_emp_insurance_billing_stg_int_count.sql
+++ tests/int_emp_insurance_billing_stg_int_count.sql
@@ -3,13 +3,12 @@ select count(*) as stg_count
 from {{ ref('stg_insurance') }} as i
 left outer JOIN
 {{ ref('stg_billing') }} as b
-ON i.insurance_id = b.billing_insurance_id
+ON i.insurance_id = b.insurance_id
 ),
-
 actual_count as (
 select count(*) as int_count
 from {{ ref('int_emp_insurance_billing') }}
 )
 
 select * from expected_count,actual_count
-where stg_count < int_count;
+where stg_count < int_count
--- tests/int_hospital_physicians_phonenumber.sql
+++ tests/int_hospital_physicians_phonenumber.sql
@@ -4,4 +4,4 @@ FROM {{ ref('int_hospital_physicians') }}
 WHERE
     LENGTH(HOSPITAL_PHONE_NUMBER) != 10 OR
     LENGTH(EMERGENCY_CONTACT) != 10 OR
-    LENGTH(PHONE_NUMBER) != 10;
+    LENGTH(PHONE_NUMBER) != 10
